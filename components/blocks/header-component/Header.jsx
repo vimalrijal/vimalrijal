09ebf0a0eb7b1106f962b0b6f55bef05a291437b
vimal rijal <106002509+vimalrijal@users.noreply.github.com>
2023-02-04 12:42:57 +0000

    image name chenage

--- components/blocks/header-component/Header.jsx
+++ components/blocks/header-component/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import "./header.css";
 import CTA from "./CTA";
-import ME from "../../../public/assets/me.PNG";
+import ME from "../../../public/assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 import Image from "next/image";
 const Header = () => {
